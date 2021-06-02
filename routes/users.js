8e4286bb5127f10a8954704e045edea23211281b
John Schaub <schaubjs@gmail.com>
2021-06-01 21:05:05 -0600

    Week 3 Assignment

--- routes/users.js
+++ routes/users.js
@@ -60,12 +60,6 @@ router.post('/signup', (req, res) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'application/json');
-	res.json({ success: true, status: 'You are successfully logged in.' });
-});
-
-router.post('/login', passport.authenticate('local'), (req, res) => {
 	const token = authenticate.getToken({ _id: req.user._id });
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'application/json');
